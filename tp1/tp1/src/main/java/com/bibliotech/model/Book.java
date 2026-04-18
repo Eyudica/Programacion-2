5abf944b82efe6bc4bb17d6e3ea96fba3e202c9d
Eyudica <e.yudica@alumno.um.edu.ar>
2026-04-18 19:49:45 -0300

    Remove files from main

--- tp1/tp1/src/main/java/com/bibliotech/model/Book.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.bibliotech.model;
-
-public record Book(String isbn, String name, String autor, String category,int stock) implements Resource{
-
-    public Book withUpdatedStock(int newStock){
-        return new Book(this.isbn,this.name,this.autor,this.category,newStock);
-    }
-}
--- tp1/tp1/src/main/java/com/bibliotech/model/Resource.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.bibliotech.model;
-
-public interface Resource { //esta creando con el () los metodos de acceso, que el record va a generar automaticamente por mi
-    String isbn();
-    String name();
-    String autor();
-    String category();
-    int stock();
-}
